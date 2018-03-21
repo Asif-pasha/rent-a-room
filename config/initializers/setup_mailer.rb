83e8030a42b4fb83b5147bc7f39d741fc4539739
Asif pasha <asifpashasdl@gmail.com>
2018-03-21 11:02:40 +0530

    capistrano gem used

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "rortraining.welt@gmail.com",
-   :password => "secret123#@!",
+   :user_name => "asifpashasdl@gmail.com",
+   :password => "44444haiasifhai@",
    :openssl_verify_mode  => 'none'
 }
